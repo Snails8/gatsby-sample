cdd6b84324b408ca966f9ae90e8d183d0b6e4f08
Kaito Suzuki <snails8d@gmail.com>
2022-09-30 09:20:27 +0900

    add: styleの方法検証

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
+import "../styles/style.css"
 
 export default function Home() {
-  return <div>Hello from Gatsby sample!</div>
+  return <div className="message">Hello from Gatsby sample!</div>
 }
